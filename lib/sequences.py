07467b598713770d3efe5a8f1da4924cc6dc2869
Olayanju Oluwadamilare <hadreytechsolutions@gmail.com>
2023-10-04 13:23:07 +0100

    Completed Assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+# def print_fibonacci(length):
+#     pass
+
+
+def printFibonacciNumbers(n):
+
+	f1 = 0
+	f2 = 1
+	if (n < 0):
+		return
+	print(f1)
+ 
+	for x in range(0, n):
+		print(f2,)
+		next = f1 + f2
+		f1 = f2
+		f2 = next
+
+
+printFibonacciNumbers(8)
+
+
+
+
